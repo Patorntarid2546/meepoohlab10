7e68d4a4dd5a003288a31fc11af58227af1d1847
Patorntarid2546 <chpooha@gmail.com>
2021-12-14 17:26:39 +0700

    finish

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+void printO(int x,int y){
+    if ((x <= 0) || (y <= 0)){
+		cout << "Invalid input";
+	}
+	else{
+      for(int i =1 ;i<=x ;i++){
+        for(int j = 1;j <= y;j++){
+            cout << 'O' ;
+        }
+        cout << "\n";
+      }
+    }
+}
+
 //Write the function printO() here
 
 int main(){
